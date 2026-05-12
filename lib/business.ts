4f7b4a3b5e2c7941e7e3e32cc3ce5b0c4d7a82b7
The Consulting Crew - Design & Tech <rudraconsultants.tax@gmail.com>
2026-05-12 13:01:29 +0530

    Add shared business data

---
+++ lib/business.ts
@@ -0,0 +1,17 @@
+export const business = {
+  brand: "The Baisa",
+  tagline: "Go Traditional",
+  location: "Sanganer, Jaipur, Rajasthan, India",
+  website: "baisajaipur.in",
+  address: "Sanganer, Jaipur, Rajasthan 303902",
+  phone: "+91 9251022710",
+  whatsapp: "+91 9251022710",
+  whatsappUrl: "https://wa.me/919251022710",
+  instagram: "@the_baisa",
+  instagramUrl: "https://instagram.com/the_baisa",
+  timings: "Mon-Sat 10am to 7pm",
+  announcement:
+    "Free Shipping Above Rs.1499 | Authentic Sanganer-Made Jaipur Ethnic Wear | Sizes XS-8XL",
+  footerAbout:
+    "The Baisa is a premium ethnic wear brand from Sanganer, Jaipur. We create authentic handcrafted clothing inspired by Rajasthani textile heritage."
+};
